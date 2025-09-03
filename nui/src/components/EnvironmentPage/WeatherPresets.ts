913f5fea578cfa5e47d51fc06b9129ef5564e723
Yorick <125297057+yorick2002@users.noreply.github.com>
2025-09-03 22:19:16 +0200

    feat(environment): add weatherPresets for various weather conditions

---
+++ nui/src/components/EnvironmentPage/WeatherPresets.ts
@@ -0,0 +1,8 @@
+export const weatherPresets = [
+  { label: 'Sunny', value: 'sunny' },
+  { label: 'Cloudy', value: 'cloudy' },
+  { label: 'Rainy', value: 'rainy' },
+  { label: 'Thunder', value: 'thunder' },
+  { label: 'Snowy', value: 'snowy' },
+  { label: 'Halloween', value: 'halloween' },
+];
